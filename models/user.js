75cd38c7816c9b8e072bc97f54445c30c7611b82
heavypete <70510263+heavypete@users.noreply.github.com>
2021-12-07 22:55:09 +0100

    hash added

--- models/user.js
+++ models/user.js
@@ -5,9 +5,10 @@ const UserSchema = new mongoose.Schema(
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     username: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: false },
     accessGroups: { type: String, required: true },
-    password: { type: String, required: true },
+    hash: { type: String, required: true },
+    password: { type: String, required: false },
   },
   {
     timestamps: true,
