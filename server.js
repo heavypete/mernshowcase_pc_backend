813fe0921b81d5e8c4b554ad5ef5d03a74adba22
heavypete <70510263+heavypete@users.noreply.github.com>
2022-01-12 14:26:30 +0100

    fixed port origin issue

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect(mongoConnectionString);
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
